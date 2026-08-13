d303552b610c95b9672b1d66301ed0dd826216ed
femboyPig <harimassaa@gmail.com>
2026-08-13 20:25:54 +0300

    Handle iOS 26 hardware keyboard events safely

--- Natives/input/KeyboardInput.m
+++ Natives/input/KeyboardInput.m
@@ -6,6 +6,20 @@
 @implementation KeyboardInput
 
 int keycodeTable[UIKeyboardHIDUsageKeyboardRightGUI+1];
+static BOOL pressedKeyTable[UIKeyboardHIDUsageKeyboardRightGUI+1];
+
+static NSUInteger keyboardTableSize(void) {
+    return sizeof(keycodeTable) / sizeof(keycodeTable[0]);
+}
+
+static char currentKeyboardModifiers(void) {
+    char modifiers = 0;
+    if (pressedKeyTable[UIKeyboardHIDUsageKeyboardCapsLock]) modifiers |= GLFW_MOD_CAPS_LOCK;
+    if (pressedKeyTable[UIKeyboardHIDUsageKeyboardLeftShift] || pressedKeyTable[UIKeyboardHIDUsageKeyboardRightShift]) modifiers |= GLFW_MOD_SHIFT;
+    if (pressedKeyTable[UIKeyboardHIDUsageKeyboardLeftAlt] || pressedKeyTable[UIKeyboardHIDUsageKeyboardRightAlt]) modifiers |= GLFW_MOD_ALT;
+    if (pressedKeyTable[UIKeyboardHIDUsageKeyboardLeftControl] || pressedKeyTable[UIKeyboardHIDUsageKeyboardRightControl]) modifiers |= GLFW_MOD_CONTROL;
+    return modifiers;
+}
 
 + (void)initKeycodeTable {
     for (int i = UIKeyboardHIDUsageKeyboardA; i <= UIKeyboardHIDUsageKeyboardZ; i++) {
@@ -64,6 +78,14 @@ int keycodeTable[UIKeyboardHIDUsageKeyboardRightGUI+1];
     keycodeTable[UIKeyboardHIDUsageKeyboardHyphen] = GLFW_KEY_MINUS;
     keycodeTable[UIKeyboardHIDUsageKeyboardEqualSign] = GLFW_KEY_EQUAL;
     keycodeTable[UIKeyboardHIDUsageKeyboardSemicolon] = GLFW_KEY_SEMICOLON;
+    keycodeTable[UIKeyboardHIDUsageKeyboardQuote] = GLFW_KEY_APOSTROPHE;
+    keycodeTable[UIKeyboardHIDUsageKeyboardEnd] = GLFW_KEY_END;
+    keycodeTable[UIKeyboardHIDUsageKeyboardInsert] = GLFW_KEY_INSERT;
+    keycodeTable[UIKeyboardHIDUsageKeyboardPrintScreen] = GLFW_KEY_PRINT_SCREEN;
+    keycodeTable[UIKeyboardHIDUsageKeyboardPause] = GLFW_KEY_PAUSE;
+    keycodeTable[UIKeyboardHIDUsageKeyboardLeftGUI] = GLFW_KEY_LEFT_SUPER;
+    keycodeTable[UIKeyboardHIDUsageKeyboardRightGUI] = GLFW_KEY_RIGHT_SUPER;
+    keycodeTable[UIKeyboardHIDUsageKeypadPeriod] = GLFW_KEY_NUMPAD_DECIMAL;
 
     // Lock keys
     keycodeTable[UIKeyboardHIDUsageKeyboardCapsLock] = GLFW_KEY_CAPS_LOCK;
@@ -105,15 +127,24 @@ int keycodeTable[UIKeyboardHIDUsageKeyboardRightGUI+1];
         modifiers |= GLFW_MOD_CONTROL;
     }
 
-    // send the keycode
-    int keycode = keycodeTable[key.keyCode];
-    if (keycode != 0) {
+    NSUInteger hidCode = key.keyCode;
+    if (hidCode >= keyboardTableSize()) {
+        NSLog(@"KeyboardInput: Ignoring out-of-range HID key %lu", (unsigned long)hidCode);
+        return NO;
+    }
+
+    int keycode = keycodeTable[hidCode];
+    BOOL stateChanged = pressedKeyTable[hidCode] != isDown;
+    pressedKeyTable[hidCode] = isDown;
+    if (keycode != 0 && stateChanged) {
         CallbackBridge_nativeSendKey(keycode, 0 /* scancode */, isDown, modifiers);
-    } else {
-        NSLog(@"KeyboardInput: Unhandled key %lu", (unsigned long)key.keyCode);
+    } else if (keycode == 0) {
+        NSLog(@"KeyboardInput: Unhandled key %lu", (unsigned long)hidCode);
     }
 
     // key.characters.length < 11: skip sending characters if the string starts with UIKeyInput
+    // GameController has no layout-aware text, so let UIKit deliver characters
+    // even when it follows a de-duplicated GC key event.
     if (isDown && key.characters.length < 11) {
         for (int i = 0; i < key.characters.length; i++) {
             int keychar = [key.characters characterAtIndex:i];
@@ -123,6 +154,36 @@ int keycodeTable[UIKeyboardHIDUsageKeyboardRightGUI+1];
     }
 
     return keycode != 0 || isDown;
+}
+
++ (BOOL)sendGCKeyCode:(NSInteger)keyCode down:(BOOL)isDown {
+    if (keyCode < 0 || (NSUInteger)keyCode >= keyboardTableSize()) {
+        NSLog(@"KeyboardInput: Ignoring out-of-range GC key %ld", (long)keyCode);
+        return NO;
+    }
+
+    NSUInteger hidCode = (NSUInteger)keyCode;
+    int glfwKey = keycodeTable[hidCode];
+    if (glfwKey == 0) {
+        NSLog(@"KeyboardInput: Unhandled GC key %ld", (long)keyCode);
+        return NO;
+    }
+
+    // UIKit and GameController can report the same physical event on iOS 26.
+    // Keep one shared state table so it is delivered exactly once.
+    if (pressedKeyTable[hidCode] == isDown) return YES;
+    pressedKeyTable[hidCode] = isDown;
+    CallbackBridge_nativeSendKey(glfwKey, 0, isDown, currentKeyboardModifiers());
+    return YES;
+}
+
++ (void)resetPressedKeys {
+    for (NSUInteger hidCode = 0; hidCode < keyboardTableSize(); hidCode++) {
+        if (pressedKeyTable[hidCode] && keycodeTable[hidCode] != 0) {
+            CallbackBridge_nativeSendKey(keycodeTable[hidCode], 0, NO, 0);
+        }
+        pressedKeyTable[hidCode] = NO;
+    }
 }
 
 @end
